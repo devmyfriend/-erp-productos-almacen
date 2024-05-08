f96169397b3688f68459d2ac8b20255b3816428d
Horacio <151089759+devmfcancun@users.noreply.github.com>
2024-05-07 19:32:01 -0500

    ft schema impuestos

--- src/schemas/Impuestos/index.js
+++ src/schemas/Impuestos/index.js
@@ -24,3 +24,40 @@ export const findByIdTax = [
     .withMessage( 'No proporciono el TaxtId' )
 ]
 
+export const findByName =[
+    param( 'taxname' )
+    .isString()
+    .notEmpty()
+    .withMessage( 'No proporcion el TaxName' )
+]
+
+export const updateTaxSchema = [
+    body( 'NombreImpuesto' )
+    .notEmpty()
+    .isString()
+    .isLength( { min:3, max:50 } )
+    .withMessage( 'El campo Nombre Impuesto es obligatorio y debe de contener de 3 a 5 caracteres' ),
+    body( 'ClaveImpuesto' )
+    .notEmpty()
+    .isString()
+    .isLength( { min: 3, max: 3 } )
+    .withMessage('El campo ClaveImpuesto es obligatorio y debe de ser de 3 caracteres' ),
+    body( 'ActualizadoPor' )
+    .notEmpty()
+    .isNumeric()
+    .withMessage( 'El campo CreadoPor es obligatorio' )
+]
+
+export const disableTax = [
+    body( 'cfgImpuestoId' )
+    .isInt()
+    .notEmpty()
+    .withMessage('El campo cfgImpuestoId es obligatortio y debe ser numérico '),
+    body('BorradoPor')
+    .isInt()
+    .notEmpty()
+    .withMessage('El campo BorradoPor es obligatorio y debe ser numérico')
+]
+
+
+
